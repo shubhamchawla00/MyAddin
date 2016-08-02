fb787b3affeccefd9d145a0b2be99ac36a091382
Dale Myers <dalemy@microsoft.com>
2016-08-02 16:26:57 +0100

    Remove jquery

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,11 @@
 var _om;
 var _item;
 
-$(document).ready(function () {
-    Office.initialize = function (reason)
-    {
-    	_om = Office.context.mailbox;
-    	_item = _om.item;
-    };
-});
+Office.initialize = function (reason)
+{
+	_om = Office.context.mailbox;
+	_item = _om.item;
+};
 
 function getSubject()
 {
